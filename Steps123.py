61b892cbe44764b0c5d73e6e62fda6cf447c7ddd
natalie-s11 <teg9gu@virginia.edu>
2026-02-03 01:55:30 +0000

    Update Job for Steps123.py

--- Steps123.py
+++ Steps123.py
@@ -30,7 +30,7 @@ Job.head()
 # %%
 # Step 2: Work through the steps outlined in the examples to include the following elements:
 # Write a generic question that this dataset could address.
-# Question - College: Does the type of college influence what type of institution it is?
+# Question - College: Does the type of college (2-year/4-year) influence what type of institution it is (public/private)?
 # Question - Job: Does gender influence salary?
 
 # What is a independent Business Metric for your problem? Think about the case study examples we have discussed in class.
@@ -69,7 +69,8 @@ College['is_four_year'] = College['is_four_year'].astype('boolean')
 College.dtypes
 
 # Job
-
+# Convert salary to integer
+Job
 
 
 
